3d90522674163d9644b2e0b442dc7c74b30f373e
Carlos Lemos <carloseduardolemos@hotmail.com>
2022-04-29 11:11:47 -0300

    add forms

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,17 @@ class PagesController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+   @user = User.new
+  end
+
+  def create
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   get "users", to: "pages#index"
+  get "users/new", to: "pages#new"
+  get "users/:id", to: "pages#show"
+  post "users", to: "pages#create"
 end
