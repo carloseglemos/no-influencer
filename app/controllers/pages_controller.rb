491b0ef150fa59c39e4c47741ee509b26fe0a72c
Carlos Lemos <carloseduardolemos@hotmail.com>
2022-04-30 20:45:05 -0300

    saturday commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,31 @@ class PagesController < ApplicationController
   end
 
   def create
+  @user = User.new(user_params)
+  @user.save
+  redirect_to user_path(@user)
+  end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+  params.require(:user).permit(:name, :city, :email)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "users", to: "pages#index"
   get "users/new", to: "pages#new"
-  get "users/:id", to: "pages#show"
+  get "users/:id", to: "pages#show", as: "user"
   post "users", to: "pages#create"
+  get "users/:id/edit", to: "pages#edit"
+  patch "users/:id", to: "pages#update"
+  delete "users/:id", to: "pages#destroy"
 end
